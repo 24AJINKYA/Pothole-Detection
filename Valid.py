0e840b1a08d251e6d660ada8d134cb76fe32e85c
24AJINKYA <157092956+24AJINKYA@users.noreply.github.com>
2024-07-29 13:41:20 +0530

    Update Valid.py

--- Valid.py
+++ Valid.py
@@ -1,7 +1,7 @@
-
-
-# Load a pre-trained YOLOv8 model
-model = YOLO('yolov8s-seg.pt')  # Using YOLOv8s for a balance between speed and accuracy
+pip install ultralytics
+from ultralytics import YOLO
+# Load a trained YOLOv8 model
+model = YOLO('best.pt')  # Give path og best.pt
 # Path to your custom dataset
 #the data.yaml file consists of the info about the annotated dataset
 data_path = "data.yaml"
