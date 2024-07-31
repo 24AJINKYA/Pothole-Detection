f9bf59ae2437a82a6ff8188750747b507c487884
24AJINKYA <157092956+24AJINKYA@users.noreply.github.com>
2024-07-31 15:21:44 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 #Step 3 : Load the Model
 model = YOLO("best.pt") # Edit this with the Path of the Best.pt file 
 # pass the necessary Args.
-results = model(source="image.png", save=True, conf=0.2) # give to the path of your test image 
+results = model(source="image.png", save=True, conf=0.2, show=True) # give to the path of your test image 
 
 print(results)
 # You will be getting the result image Stored in the 'runs folder'
