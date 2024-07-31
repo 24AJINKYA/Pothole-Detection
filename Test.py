09e3081d59f49116d88cd6beef859e5d603755d6
24AJINKYA <157092956+24AJINKYA@users.noreply.github.com>
2024-07-31 15:17:02 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,8 +5,7 @@ from ultralytics import YOLO
 
 #Step 3 : Load the Model
 model = YOLO("best.pt") # Edit this with the Path of the Best.pt file 
-
-# Pass only True (or False if you don't want to save) to the save argument
+# pass the necessary Args.
 results = model(source="image.png", save=True, conf=0.2) # give to the path of your test image 
 
 print(results)
